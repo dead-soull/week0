8afa0a37ad0f0301e45c15c002c202c6b054e1ff
dead-soull <ru6in4ik@gmail.com>
2025-10-09 12:55:27 +0300

    week 0 done !!

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(dollars):
+    dollars = dollars.strip("$")
+    dollars = float(dollars)
+    return dollars
+
+
+def percent_to_float(percent):
+    percent = percent.strip("%")
+    percent = float(percent) * 0.01
+    return percent
+
+
+main()
+
+
+"""
+Run your program with python tip.py. Type $50.00 and press Enter. Then, type 15% and press Enter. Your program should output:
+Leave $7.50    
+Run your program with python tip.py. Type $100.00 and press Enter. Then, type 18% and press Enter. Your program should output:
+Leave $18.00
+Run your program with python tip.py. Type $15.00 and press Enter. Then, type 25% and press Enter. Your program should output
+Leave $3.75
+"""
