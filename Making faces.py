f8f2fb62be29e0392d6f8fe2b29396ff4039f1b1
dead-soull <ru6in4ik@gmail.com>
2025-10-09 12:24:06 +0300

    added comments

--- Making faces.py
+++ Making faces.py
@@ -3,6 +3,7 @@ def main():
     print(TextToEmoji(str))
 
 def TextToEmoji(text):
+    # Not the craziest solution, but it works and I can't really find an understandable way to do it differently yet
     return text.replace(":(", "🙁").replace(":)", "🙂")
     
 main()
